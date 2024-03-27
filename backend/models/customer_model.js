cfa40db4fcd3a559501c8c23b2241649a44a3c12
Peetu Säkkinen <peetu.sakkinen@gmail.com>
2024-03-27 18:33:49 +0200

    muokkauksia

---
+++ backend/models/customer_model.js
@@ -0,0 +1,28 @@
+const db=require('../database');
+
+const customer = {
+    getAll: function(callback) {
+      return db.query('select * from customer', callback);
+    },
+    getById: function(id, callback) {
+      return db.query('select * from customer where idCustomer=?', [id], callback);
+    },
+    add: function(customer, callback) {
+      return db.query(
+        'insert into customer (fname, lname, idCustomer) values(?,?,?)',
+        [customer.fname, customer.lname, customer.idCustomer],
+        callback
+      );
+    },
+    delete: function(id, callback) {
+      return db.query('delete from customer where idCustomer=?', [id], callback);
+    },
+    update: function(id, customer, callback) {
+      return db.query(
+        'update customer set idCustomer=?, fname=?,lname=? where idCustomer=?',
+        [customer.fname, customer.lname, customer.idCustomer],
+        callback
+      );
+    }
+  };
+  module.exports = customer;
